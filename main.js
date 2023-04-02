1f6449a7536da7c8bf38db513e9c3c74667caa70
TimiMaliki <malikitimi@gmail.com>
2023-04-02 14:16:06 -0700

    done

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
 
 
-let img = document.getElementById("imgBox");
-let imgContain = document.getElementById("imgContainer");
+const img = document.getElementById("imgBox");
+const imgContain = document.getElementById("imgContainer");
 
 function zoomIn(){
-     let theBoxwidth = imgContain.clientWidth;
-     let theBoxheight = imgContain.clientHeight;
+     const theBoxwidth = imgContain.clientWidth;
+     const theBoxheight = imgContain.clientHeight;
 
-     let originwidth = img.clientWidth;
-     let originheight = img.clientHeight;
+     const originwidth = img.clientWidth;
+     const originheight = img.clientHeight;
     
 
-     imgContain.style.width = (theBoxwidth + 50) + "px"
-     imgContain.style.height = (theBoxheight + 50) + "px"
+     imgContain.style.width =   `${theBoxwidth + 50}px`
+     imgContain.style.height =   `${theBoxheight + 50}px`
 
-     img.style.width = (originwidth + 50) + "px"
-     img.style.height = (originheight + 50) + "px" 
+     img.style.width =  `${originwidth + 50}px`
+     img.style.height = `${originheight + 50}px` 
      
 }
 
 
 function zoomOut(){
-    let theBoxwidth = imgContain.clientWidth;
-    let theBoxheight = imgContain.clientHeight;
+    const theBoxwidth = imgContain.clientWidth;
+    const theBoxheight = imgContain.clientHeight;
 
-    let originwidth = img.clientWidth;
-     let originheight = img.clientHeight;
+    const originwidth = img.clientWidth;
+     const originheight = img.clientHeight;
     
 
      imgContain.style.width = (theBoxwidth - 50) + "px"
